6dd716939ad1f0e32985d5c81ee9da8b4f8b1760
qahSgiB <tomas.sumsala@gmail.com>
2019-07-23 07:35:52 +0200

    FCTRL2 solution added

--- FCTRL2/main.rkt
+++ FCTRL2/main.rkt
@@ -1,26 +1,11 @@
 #lang racket
 
-
-
 (define (input)
     (read-line (current-input-port) 'any)
 )
 
 (define (factorial n)
-    (define zeros 0)
-
-    (for ([power (in-range 1 13)]) (
-        (let ([f (expt 5 power)]))
-
-        (if (<= f n) (
-            (set! power (add1 power))
-            (set! zeros (floor (/ n f)))
-        ) (
-            (set! nEnd #f)
-        ))
-    ))
-
-    zeros
+    (if (equal? n 1) 1 (* n (factorial (- n 1))))
 )
 
 
